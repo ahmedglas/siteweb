190f9d98fb6ca4a8c1355a3d3ae3bce56f359de9
uvsq21919049 <ahmed.saadi@ens.uvsq.fr>
2019-12-16 11:41:13 +0100

    aaa

--- add_carte.php
+++ add_carte.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id']))
                          $user_id=$_SESSION['user_id']; 
                          $id_article= $_GET['id_article'];
                             $qte=$_GET['quantite'];
-                            echo "hahahah".$qte
+                           
 ;						
 						$sql = "INSERT into apartient values
                         ($id_article, ( SELECT commande_id from commande where etatcmd like 'en%' and user_id ='".$user_id."'),
